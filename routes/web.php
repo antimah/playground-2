03b391e395262c470c30072fab188bbbfb973eda
Tyunay Kamber <kamber@gmx.com>
2021-08-25 02:59:33 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::post('logout',[SessionController::class,'destroy'])->middleware('auth');
 Route::get('login',[SessionController::class,'create'])->middleware('guest');
 Route::post('login',[SessionController::class,'store'])->middleware('guest');
 
-
+// testing
 
 
 
